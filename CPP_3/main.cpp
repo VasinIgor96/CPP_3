0f17a79d3294b69b5bb8a5c7a2644449a6b439c5
Vasin Igor <vasin.igor0796@gmail.net>
2023-05-14 21:47:54 +0300

    Task done

---
+++ CPP_3/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "╔════════════════════════════════════════════╗" << endl;
+    cout << "║   Pory Roku                                ║" << endl;
+    cout << "╠═════════╦═══════════╦═══════════╦══════════╣" << endl;
+    cout << "║   Zyma  ║   Vesna   ║   Lito    ║   Osin   ║" << endl;
+    cout << "╚═════════╩═══════════╩═══════════╩══════════╝" << endl;
+
+    return 0;
+}
