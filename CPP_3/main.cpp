04da0920855a0061f24a939d925d54d27310e3e3
Vasin Igor <vasin.igor0796@gmail.net>
2023-05-14 21:37:38 +0300

    Task done

---
+++ CPP_3/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "The war and the peace";
+    string author = "L.N. Tolstoj";
+    string publisher = "Piter";
+    int pages = 500;
+
+    cout << "Name: \"" << name << "\"" << endl;
+    cout << "Author: " << author << endl;
+    cout << "Publisher: " << publisher << endl;
+    cout << "Pages: " << pages << endl;
+
+    return 0;
+}
