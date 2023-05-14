d73f14a9a47603a086a6d4d92616ddafe70947c1
Vasin Igor <vasin.igor0796@gmail.net>
2023-05-14 21:43:21 +0300

    Task done

---
+++ CPP_3/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main() {
+    Beep(500, 500);
+    Beep(500, 500);
+    Beep(500, 500);
+
+    cout << "Kraj lukomor'a dub zelenyj,\n";
+    Beep(600, 500);
+    Beep(600, 500);
+    Beep(600, 500);
+
+    cout << "I zolotyj lancyuh na nim:\n";
+    Beep(700, 500);
+    Beep(700, 500);
+    Beep(700, 500);
+
+    cout << "Shodnya, shonochi kit uchenyj\n";
+    Beep(800, 500);
+    Beep(800, 500);
+    Beep(800, 500);
+
+    cout << "Na lancyuhu kruzhl'aje tim\n";
+    Beep(900, 500);
+    Beep(900, 500);
+    Beep(900, 500);
+
+    return 0;
+}
